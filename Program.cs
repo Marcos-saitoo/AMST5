20aa9d60a0fcddfa11f20ae72e656fc35be9104f
Marcos-saitoo <marcossaito33@gmail.com>
2025-03-23 13:08:44 -0300

    second_commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-""
+﻿using Car.Entities;
+    Locacao locacao = new Locacao();
+    
+ int TempoH ;
+
+
+
+   Console.WriteLine("Infome o Id ");
+locacao.ID = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Infome o instante inicial");
+locacao.IsI = double.parse(Console.ReadLine());
+Console.WriteLine("Infome o instante final ");
+locacao.IsF = double.parse(Console.ReadLine());
+Console.WriteLine("Infome o Valor da hora ");
+locacao.VH = double.parse(Console.ReadLine());
+Console.WriteLine("Infome o Modelo");
+locacao.Model = Console.ReadLine();
+int VlLocacao = (locacao.VD + locacao.VH);
+TempoH = (locacao.IsF - locacao.IsI);
+
+if (TempoH > 12 && locacao.VH <= 100){
+    Console.WriteLine("Infome o Valor Diario ");
+    locacao.VD = Convert.ToInt16(Console.ReadLine());
+   double Vt = (VlLocacao*1.20);
+   double VlImposto = (VlLocacao*0.20);
+   Console.WriteLine($"Valor da locação {VlLocacao}, Valor Total {Vt},valor do imposto{VlImposto},Modelo do carro{locacao.Model},o carro foi alugado por {TempoH} horas  ");
+
+}
+else if (TempoH > 12 && locacao.VH > 100){
+    Console.WriteLine("Infome o Valor Diario ");
+    locacao.VD = Convert.ToInt16(Console.ReadLine());
+   double Vt = (VlLocacao *1.15);
+   double VlImposto = (VlLocacao*0.15);
+   Console.WriteLine($"Valor da locação {VlLocacao}, Valor Total {Vt},valor do imposto{VlImposto},Modelo do carro{locacao.Model}, o carro foi alugado por {TempoH} horas  ");
+
+}
+else 
+     Console.WriteLine($"Valor da locação {VlLocacao}, Valor Total {VlLocacao},Modelo do carro{locacao.Model}, o carro foi alugado por {TempoH} horas ");
+
+ 
---
+++ entities/Car.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car.Entities
+{
+    public class Locacao
+    {
+        public int ID {get;  set;}
+        public double IsF{get; set;}
+        public double IsI{get; set;} 
+        public double VH {get; set;}
+        public double VD {get; set;}
+        public string Model {get; set;}
+
+        
+
+        
+    }
+}
